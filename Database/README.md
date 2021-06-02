93ef12899611f78e918ba7cc3d77ac7688582f6b
Shabanov Roman <shabanov.roman0@gmail.com>
2021-06-02 19:18:40 +0800

    Add database for project

---
+++ Database/README.md
@@ -0,0 +1,29 @@
+<h2>Схема БД</h2>
+
+<img src="./bd_scheme.png">
+
+<section>
+    <table>
+        <caption><b>Таблицы и их описание</b></caption>
+        <tr>
+            <td>Название таблицы</td>
+            <td>Описание таблицы</td>
+        </tr>
+        <tr>
+            <td>task_types</td>
+            <td>Названия типов задач</td>
+        </tr>
+        <tr>
+            <td>task_statuses</td>
+            <td>Названия статусов задач</td>
+        </tr>
+        <tr>
+            <td>users</td>
+            <td>Информация о пользователях</td>
+        </tr>
+        <tr>
+            <td>tasks</td>
+            <td>Информация о задачах</td>
+        </tr>
+    </table>
+</section>
---
+++ Database/init.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS `task_types` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`name`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `task_statuses` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`name`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`name`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `type_id` INT UNSIGNED NOT NULL,
+    `location` VARCHAR(255),
+    `time` TIMESTAMP NOT NULL,
+    `duration` TIME NOT NULL DEFAULT '10000',
+    `text` TEXT,
+    `status_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`type_id`) REFERENCES `task_types` (`id`)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY (`status_id`) REFERENCES `task_statuses` (`id`)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
